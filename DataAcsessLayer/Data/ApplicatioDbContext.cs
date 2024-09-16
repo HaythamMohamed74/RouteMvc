6b321af907632ec7b42109eab36e5e4797694361
HaythamMohamed74 <95954085+HaythamMohamed74@users.noreply.github.com>
2024-09-16 14:24:21 +0300

    DAL for Departmnet

---
+++ DataAcsessLayer/Data/ApplicatioDbContext.cs
@@ -0,0 +1,30 @@
+﻿using DataAcsessLayer.Data.Configurations;
+using DataAcsessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcsessLayer.Data
+{
+    public class ApplicatioDbContext:DbContext
+    {
+
+        public DbSet<Department> Departments { get; set; }
+
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-662AKIO\\SQLEXPRESS;Initial Catalog=MVCApplication;Integrated Security=True;Trust Server Certificate=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+
+}
---
+++ DataAcsessLayer/Data/Configurations/DepartmentConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using DataAcsessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAcsessLayer.Data.Configurations
+{
+    public class DepartmentConfigurations : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+            builder.Property(d => d.Id).UseIdentityColumn(1,1);
+            builder.Property(d => d.Name).HasColumnType("nvarchar").HasMaxLength(50).IsRequired();
+            builder.Property(d => d.Code).HasColumnType("nvarchar").HasMaxLength(100).IsRequired();
+
+        }
+    }
+}
